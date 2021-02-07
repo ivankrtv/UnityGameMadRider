92ac4a4cb21499941a30b4fa3f64bab22e73964b
ivankrtv <mega.krotov@mail.ru>
2021-02-07 17:30:30 +0300

    go to 3D models, fix convas

---
+++ Assets/Scripts/MainMenuRoadMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MainMenuRoadMove : MonoBehaviour
+{
+    public static float speed = 20f;
+    private float tp = 42.3f;
+    private float z;
+    private Vector3 vec = new Vector3(0, 0, 1);
+
+
+    void Update()
+    {
+        if (speed >= 0)
+        {
+            z = transform.position.z;
+            if (z > -16.3)
+            {
+                transform.Translate(-speed * vec * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(tp * vec);
+            }
+        }
+    }
+}
